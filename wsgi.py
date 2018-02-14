cfefccd29b1f3e70464a08455e670314efdee37e
Nahuel Persia <npersia@npersia.notebook>
2018-02-14 12:02:32 -0300

    todo

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,47 @@
+import requests
+import issues
 from flask import Flask
-application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
-    application.run()
+
+
+__headers__ = {"X-Redmine-API-Key": "1e8a0e5eff9a6c4311daac0f0f69b20fd00f765b"}
+
+__url_base__ = "https://redmine.semperti.com"
+
+
+def getRedMine(url,headers=__headers__):
+    return requests.get(url,headers=headers).json()
+
+
+def getAllIssues():
+    url = __url_base__ + "/issues.json"
+    return getRedMine(url)
+
+
+def getIssuesUpdate():
+    r = getAllIssues()
+    issues = {}
+    for x in r["issues"]:
+        issues[x["id"]] = x["updated_on"]
+    return issues
+
+
+def getIssueRedMine(issue):
+    url = __url_base__ + "/issues/" + issue + ".json?include=journals"
+    return getRedMine(url)
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/issue/<issue_id>')
+def getIssueid(issue_id):
+    return str(getIssueRedMine(issue_id))
+
