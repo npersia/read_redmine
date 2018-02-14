c940ef816db2294d82397b07177611fed9eb3ebe
Nahuel Persia <npersia@npersia.notebook>
2018-02-14 12:20:55 -0300

    todo

--- config.py
+++ config.py
@@ -1,15 +1,5 @@
 import os
 
-import pip
-
-def install(package):
-    pip.main(['install', package])
-
-# Example
-if __name__ == '__main__':
-    install('requests')
-
-
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
